e3d2d6d005d0e8b3bab6b58cb00ac83d3c4113b0
Oren Hartstein <orenh2020@gmail.com>
2023-09-06 22:34:36 -0700

    Added add() method

--- Git.java
+++ Git.java
@@ -1,13 +1,25 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Git {
     public Git(){
        
     }
 
-    public void init(){
+    public void init() throws IOException{
         File indexFile = new File("index.txt");
         File objFolder = new File("objects");
         objFolder.mkdir();
+    }
+    
+    public void add(String fileName) throws Exception{
+        Blob blob = new Blob(fileName);
+        String hash = blob.getHash();
+        BufferedWriter bw = new BufferedWriter(new FileWriter("index.txt"));
+        bw.write(fileName + ": " + hash);
+        bw.newLine();
+        bw.close();
     }
 }
