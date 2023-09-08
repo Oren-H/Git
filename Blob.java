21c9e7d9a333920a369db0535100482c94d939e7
Oren Hartstein <orenh2020@gmail.com>
2023-09-07 20:50:31 -0700

    Replaced Sha1 hasher method

--- Blob.java
+++ Blob.java
@@ -1,8 +1,11 @@
 import java.io.File;
 import java.io.FileWriter;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Blob{
 
@@ -20,14 +23,23 @@ public class Blob{
     }
 
     //hashes an array of bytes to a string using the Sha1 hash function
-    public static String getStringHash(String contents) {
-        byte[] b = contents.getBytes();
-        String result = "";
-        for (int i=0; i < b.length; i++) {
-          result +=
-                Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+    public static String getStringHash(String input)
+    {
+        try {
+            // getInstance() method is called with algorithm SHA-1
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
         }
-        return result;
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getBlobHash(){
