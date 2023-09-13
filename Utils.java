404a13a6776df19921d2d5c1479e7096d45961b6
Luke Collins <luke23571113@gmail.com>
2023-09-13 09:58:27 -0700

    added Utils

---
+++ Utils.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*; 
+
+public class Utils {
+    public static String readFileToString (String fileName) throws Exception
+    {
+        BufferedReader br = new BufferedReader (new FileReader(fileName));
+
+        StringBuilder sb = new StringBuilder(fileName);
+
+        while (br.ready())
+        {
+            sb.append((char) br.read());
+        }
+
+        br.close();
+
+        return sb.toString();
+    }
+}
