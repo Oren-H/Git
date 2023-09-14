427f8d7af93aed4d77cac6ec56500620c4608c3b
Luke Collins <luke23571113@gmail.com>
2023-09-13 22:18:05 -0700

    add and remove methods

--- Tree.java
+++ Tree.java
@@ -5,11 +5,18 @@ public class Tree {
 
     public Tree ()
     {
-
+        contents = new StringBuilder();
     }    
 
     public void add (String entry) //assume its a proper entry
     {
+        //check if the entry already exists
+        String s = contents.toString(); 
+        if (s.contains(entry))
+        {
+            return;
+        }
+
         if (contents.isEmpty()) 
         {
             contents.append(entry);
@@ -23,6 +30,27 @@ public class Tree {
 
     public void remove (String s)
     {
+        //check if it even exists first
+        String sContent = contents.toString();
+        if (!sContent.contains(s)) 
+        {
+            return;
+        }
 
+        StringBuilder newContent = new StringBuilder();
+
+        Scanner sc = new Scanner(sContent); //use scanner to loop through string line by line
+
+        while (sc.hasNextLine())
+        {
+            //only add the currentline to the stringbuilder if it doesn't contain string s
+            String curLine = sc.nextLine();
+            if (!curLine.contains(s))
+            {
+                newContent.append(curLine);
+            }
+        }
+
+        contents = newContent; 
     }
 }
