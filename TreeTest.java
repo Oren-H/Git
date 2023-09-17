b589886fbc866da50faaaafc1a89d873de45c8d2
Luke Collins <luke23571113@gmail.com>
2023-09-17 15:38:27 -0700

    added Tree test writeToFile

--- TreeTest.java
+++ TreeTest.java
@@ -49,12 +49,41 @@ public class TreeTest {
 
     @Test
     void testRemove() {
-        File f = new File ("temp.txt");
-        assertTrue(f.exists());
+
+        Tree tree = new Tree();
+        
+        String entry1 = "tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b";
+        String entry2 = "blob : 81e0268c84067377a0a1fdfb5cc996c93f6dcf9f : file1.txt";
+
+        tree.add(entry1);
+        tree.add(entry2);
+
+        tree.remove("file1.txt");
+        assertFalse ("Did not remove file correctly", tree.getContents().contains (entry2));
+
+        tree.remove("bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b");
+        assertFalse ("Did not remove tree correctly", tree.getContents().contains (entry1));
     }
 
     @Test
-    void testWriteToFile() {
+    void testWriteToFile() throws Exception {
+        Tree tree = new Tree();
+        
 
+        tree.add("blob : 81e0268c84067377a0a1fdfb5cc996c93f6dcf9f : file1.txt");
+        tree.add("blob : 01d82591292494afd1602d175e165f94992f6f5f : file2.txt");
+        tree.add("blob : f1d82236ab908c86ed095023b1d2e6ddf78a6d83 : file3.txt");
+        tree.add("tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b");
+        tree.add("tree : e7d79898d3342fd15daf6ec36f4cb095b52fd976");
+
+        tree.writeToFile();
+
+        //test if the file is created in objects
+        String hash = Blob.getStringHash(tree.getContents());
+        File treeFile = new File ("./objects/" + hash);
+        assertTrue ("Tree does not write to correct SHA file", treeFile.exists());
+
+        //then check to see if all the information is copied correctly
+        assertTrue ("Tree Sha file does not contain the correct information", Utils.readFileToString("./objects/" + hash).equals(tree.getContents()));
     }
 }
