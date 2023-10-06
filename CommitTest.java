c8b9dfe85a24225bd44c861e4ed34c864dbc992a
Oren Hartstein <orenh2020@gmail.com>
2023-10-05 19:14:23 -0700

    MINIMUM VIABLE REQUIREMENTS COMPLETE

--- CommitTest.java
+++ CommitTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class CommitTestNew {
+public class CommitTest {
     @BeforeAll
     static void runBeforeAll() throws IOException{
         Git.init();
